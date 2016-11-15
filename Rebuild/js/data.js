5ae7a65fcc66aea68a3baba8954ce7eebdab7851
salhussa <sarah.alhussaini@gmail.com>
2016-11-15 18:29:12 +0000

    push

---
+++ Rebuild/js/data.js
@@ -0,0 +1,28 @@
+[
+	 "Organisations":
+	 [
+		{
+			"name": "UNICEF",
+			"desciption": "blahblah",
+			"logo": "logo",
+			"URL": "URL",
+			"where": "London",
+			"who": ["student", "techie"],
+			"how": ["donation"]  
+
+		},
+		{
+			"name": "Save the Children",
+			"where": "Manchester",
+			"who": ["NGO", "other"],
+			"how": ["events"]  
+
+		},
+		{
+			"name": "One to One",
+			"where": "Bristol",
+			"who": ["Young Professional", "techie", "NGO"],
+			"how": ["donation", "fundraising", "events"]  
+		}
+	]
+]
